f4789f1edf7402da8cf0ee02b7f6485969432d2b
Juandi-M <juanmonge95@gmail.com>
2023-10-06 16:37:11 -0600

    offset time, improvements.

--- windows/main.py
+++ windows/main.py
@@ -1,12 +1,11 @@
-# Import required libraries and modules
 import tkinter as tk
 from tkinter import ttk
 import threading
 import time
+import datetime
 import pyautogui
 import logging
-import datetime
-import ctypes  # For Windows to prevent sleep
+import ctypes  # For preventing Windows from going to sleep
 
 # Initialize logging for debugging and tracking
 def init_logger():
@@ -16,48 +15,47 @@ def init_logger():
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-# Function to update elapsed time on the GUI
-def update_time(start_time, time_var):
-    elapsed_time = datetime.datetime.now() - start_time
-    time_var.set(f"Running for {str(elapsed_time).split('.')[0]}")
-    root.after(1000, update_time, start_time, time_var)
+# Function to update elapsed time
+def update_time(start_perf_counter, time_var, stop_event):
+    while not stop_event.is_set():
+        elapsed_time = time.perf_counter() - start_perf_counter
+        time_var.set(f"Running for {datetime.timedelta(seconds=int(elapsed_time))}")
+        time.sleep(1)
 
 # Function to move the mouse
 def move_mouse(stop_event, text_var, counter_var):
     counter = 0
+    start_perf_counter = time.perf_counter()
+    next_move_time = 6  # Initial offset for mouse movement
     while not stop_event.is_set():
-        try:
-            # Move mouse 10 pixels down
-            pyautogui.moveRel(0, 10)
-            # Wait for 0.5 seconds
-            time.sleep(0.5)
-            # Move mouse 10 pixels up
-            pyautogui.moveRel(0, -10)
-            # Increment the movement counter
-            counter += 1
-            counter_var.set(f"Mouse has moved {counter} times")
-            text_var.set("Mouse moving")
-            logging.info("Mouse moved successfully.")
-            # Wait for 5 seconds before the next movement
-            time.sleep(5)
-        except pyautogui.FailSafeException:
-            logging.warning("FailSafeException triggered, moving mouse to center.")
-            text_var.set("FailSafeException, moved to center")
-            stop_event.set()
+        elapsed_time = time.perf_counter() - start_perf_counter
+        if elapsed_time >= next_move_time:
+            try:
+                pyautogui.moveRel(0, 10)
+                time.sleep(0.5)
+                pyautogui.moveRel(0, -10)
+                counter += 1
+                counter_var.set(f"Mouse has moved {counter} times")
+                text_var.set("Mouse moving")
+                logging.info("Mouse moved successfully.")
+                next_move_time += 6  # Schedule the next move
+            except pyautogui.FailSafeException:
+                logging.warning("FailSafeException triggered, moving mouse to center.")
+                text_var.set("FailSafeException, moved to center")
+                stop_event.set()
+        time.sleep(0.1)  # Sleep for a short while before checking again
 
-# Main function to create and control the GUI
+# Main function for GUI
 def main():
-    # Initialize the logger
     init_logger()
 
-    # Function to prevent Windows from going to sleep
+    # Prevent Windows from going to sleep
     ES_CONTINUOUS = 0x80000000
     ES_SYSTEM_REQUIRED = 0x00000001
     ctypes.windll.kernel32.SetThreadExecutionState(
         ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
 
-    # Set up the Tkinter GUI
-    global root
+    # Tkinter GUI setup
     root = tk.Tk()
     root.title("Mouse Mover for Windows")
 
@@ -83,16 +81,14 @@ def main():
 
     def start_moving():
         stop_event.clear()
-        start_time = datetime.datetime.now()
-        update_time(start_time, time_var)
+        start_perf_counter = time.perf_counter()
+        threading.Thread(target=update_time, args=(start_perf_counter, time_var, stop_event)).start()
         threading.Thread(target=move_mouse, args=(stop_event, text_var, counter_var)).start()
-        logging.info("Mouse movement started.")
 
     def stop_moving():
         stop_event.set()
         text_var.set("Mouse stopped")
         counter_var.set("Mouse has not moved yet")
-        logging.info("User stopped the mouse movement.")
 
     start_button = ttk.Button(frame, text="Start", command=start_moving)
     start_button.grid(row=3, column=0)
